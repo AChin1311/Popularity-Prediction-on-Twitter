e7eac6e1729002573006457dca0d2f26aec9e6e6
AChin1311 <daphne011552000@yahoo.com.tw>
2018-03-10 02:44:31 -0800

    daphne is so drunk

--- Q1_1.py
+++ Q1_1.py
@@ -2,6 +2,7 @@ import json
 import datetime, time
 import pytz
 import matplotlib.pyplot as plt 
+import collections
 
 def Q1_1_1(filenames=["tweets_#gohawks.txt", "tweets_#nfl.txt", "tweets_#sb49.txt", "tweets_#gopatriots.txt", "tweets_#patriots.txt", "tweets_#superbowl.txt"]):
   for fname in filenames: 
@@ -9,6 +10,20 @@ def Q1_1_1(filenames=["tweets_#gohawks.txt", "tweets_#nfl.txt", "tweets_#sb49.tx
     followers_num = 0
     retweets_num = 0
     line = 0
+
+    # init tw_per_hour start from 1/14/00:00 to 2/7/10:00
+    for i in range(14, 32):
+      for j in range(24):
+        hr_str = 10000+(i)*100+j
+        tw_per_hour[hr_str] = 0
+
+    for i in range(7): 
+      for j in range(24):
+        if i == 6 and j == 11:
+          break
+        hr_str = 20000+(i+1)*100+j
+        tw_per_hour[hr_str] = 0
+
     with open("tweet_data/"+fname) as f:
       tweets = f.readlines()
       for tw in tweets:
@@ -19,28 +34,37 @@ def Q1_1_1(filenames=["tweets_#gohawks.txt", "tweets_#nfl.txt", "tweets_#sb49.tx
         time_str = datetime.datetime.fromtimestamp(citation_date, pst_tz)
         
         post_hour = (time_str.month*100 + time_str.day)*100 + time_str.hour
-        if post_hour in tw_per_hour:
-          tw_per_hour[post_hour] += 1
-        else:
-          tw_per_hour[post_hour] = 1
+        tw_per_hour[post_hour] += 1
         
         followers_num += tw['author']['followers']
         retweets_num += tw['metrics']['citations']['total']
 
+      # od = collections.OrderedDict(sorted(tw_per_hour.items()))
+      
       print("filenames:", fname)
       print('Avg # of tweets per hour: ', sum([tw_per_hour[i] for i in tw_per_hour])/len(tw_per_hour))
       print('Avg # of followers: ', followers_num/line)
       print('Avg # of retweets: ', retweets_num/line)
-      
-      print("for checking", sum([tw_per_hour[i] for i in tw_per_hour]), line)
       print('-'*20)
 
-# , "tweets_#superbowl.txt"
-def Q1_1_2(filenames=["tweets_#nfl.txt"]):
-  import collections
+def Q1_1_2(filenames=["tweets_#nfl.txt", "tweets_#superbowl.txt"]):
+
 
   for fname in filenames: 
     tw_per_hour = {}
+    # init tw_per_hour start from 1/14/00:00 to 2/7/10:00
+    for i in range(14, 32):
+      for j in range(24):
+        hr_str = 10000+(i)*100+j
+        tw_per_hour[hr_str] = 0
+
+    for i in range(7): 
+      for j in range(24):
+        if i == 6 and j == 11:
+          break
+        hr_str = 20000+(i+1)*100+j
+        tw_per_hour[hr_str] = 0
+
     line = 0
     with open("tweet_data/"+fname) as f:
       tweets = f.readlines()
@@ -72,7 +96,8 @@ def Q1_1_2(filenames=["tweets_#nfl.txt"]):
     print(y)
 
     plt.figure()
-    plt.bar(x, y, align='center')
+    plt.bar(range(len(y)), y, align='center')
+    # plt.xticks(range(len(y)), xl, size='small')
     plt.ylabel('posts per hour')
     plt.xlabel('hour')
     plt.savefig('plot/'+fname+'_per_hour.png')
