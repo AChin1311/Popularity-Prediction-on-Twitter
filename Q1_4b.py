5171a5b05e8d216c8b60e65d0a7043512bffd39a
AChin1311 <daphne011552000@yahoo.com.tw>
2018-03-11 21:54:54 -0700

    fix bug

--- Q1_4b.py
+++ Q1_4b.py
@@ -15,6 +15,74 @@ pst_tz = pytz.timezone('US/Pacific')
 filenames = ["tweets_#gohawks.txt", "tweets_#nfl.txt", "tweets_#sb49.txt", "tweets_#gopatriots.txt", "tweets_#patriots.txt", "tweets_#superbowl.txt"]
 # filenames = ["tweets_#gohawks.txt"]
 
+def read_all_data(filenames):
+  tw_per_hour = {}
+  for i in range(14, 32):
+    for j in range(24):
+      hr_str = 10000+(i)*100+j
+      tw_per_hour[hr_str] = [0, 0, 0, 0, j]
+
+  for i in range(7): 
+    for j in range(24):
+      if i == 6 and j == 11:
+        break
+      hr_str = 20000+(i+1)*100+j
+      tw_per_hour[hr_str] = [0, 0, 0, 0, j]
+
+  for fname in filenames:
+    print("reading file ", fname)
+    with open("tweet_data/"+fname,'r') as f:
+      tweets = f.readlines()
+
+      for tw in tweets:
+        tw = json.loads(tw)
+        time_str = datetime.fromtimestamp(tw["citation_date"], pst_tz)
+        post_hour = (time_str.month * 100 + time_str.day) * 100 + time_str.hour
+        # print(time_str, post_hour)
+        rt = tw['metrics']['citations']['total']
+        followers = tw['author']['followers']
+        if post_hour in tw_per_hour:
+          tw_per_hour[post_hour][0] += 1
+          tw_per_hour[post_hour][1] += rt
+          tw_per_hour[post_hour][2] += followers
+          tw_per_hour[post_hour][3] = max(followers, tw_per_hour[post_hour][3])
+        else:
+          print("post hour not in dic ", post_hour)
+          tw_per_hour[post_hour] = [1, rt, followers, followers, post_hour%100]
+    f.close()
+
+  tw_per_hour = collections.OrderedDict(sorted(tw_per_hour.items()))
+  return seperate_data(tw_per_hour)
+
+def seperate_data(data, n=5):
+  before_event = []
+  between_event = []
+  after_event = []
+  
+  for k,v in data.items():
+    if k < 20108:
+      before_event.append(v)
+    elif k >= 20108 and k <= 20120:
+      between_event.append(v)
+    elif k > 20120:
+      after_event.append(v)
+
+  
+  before_event_X = [list(itertools.chain.from_iterable(before_event[i-n:i])) for i in range(n, len(before_event))]
+  before_event_Y = [l[0] for l in before_event[n:]]
+
+  between_event_Y = [l[0] for l in between_event]
+  between_event = before_event[-5:]+between_event
+  between_event_X = [list(itertools.chain.from_iterable(between_event[i:i+n])) for i in range(0, len(between_event)-n)]
+  
+  after_event_Y = [l[0] for l in after_event]
+  after_event = between_event[-5:]+after_event
+  after_event_X = [list(itertools.chain.from_iterable(after_event[i:i+n])) for i in range(0, len(after_event)-n)]
+  
+  return before_event_X, before_event_Y, between_event_X, between_event_Y, after_event_X, after_event_Y
+
+
+
 def linear_regr(X, Y):
   X = np.array(X)
   Y = np.array(Y)
@@ -51,16 +119,10 @@ if __name__ == "__main__":
   after_event_X = []
   after_event_Y = []
 
-  for fname in filenames:
-    before_X, before_Y, between_X, between_Y, after_X, after_Y = Q1_4.read_data(fname)
-    print(len(before_X), len(between_X), len(after_X))
-    print(len(before_Y), len(between_Y), len(after_Y))
-    before_event_X.extend(before_X)
-    before_event_Y.extend(before_Y)
-    between_event_X.extend(between_X)
-    between_event_Y.extend(between_Y)
-    after_event_X.extend(after_X)
-    after_event_Y.extend(after_Y)
+  before_event_X, before_event_Y, between_event_X, between_event_Y, after_event_X, after_event_Y = read_all_data(filenames)
+  print(len(before_event_X), len(between_event_X), len(after_event_X))
+  print(len(before_event_Y), len(between_event_Y), len(after_event_Y))
+  
 
   print("random forest regression:")
   print("before event")
